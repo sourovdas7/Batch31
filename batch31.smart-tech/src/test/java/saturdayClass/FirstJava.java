85d3469b650114fb99b8e0b87ca27489e38b573d
souro <souro@DESKTOP-IG9I2AG.home>
2022-11-19 11:32:41 -0500

    First Push

---
+++ batch31.smart-tech/src/test/java/saturdayClass/FirstJava.java
@@ -0,0 +1,10 @@
+package saturdayClass;
+
+public class FirstJava {
+public static void main(String[] args) {
+	System.out.println("HI java");
+	System.out.println("go");
+	System.out.println("Home");
+}
+}
+
